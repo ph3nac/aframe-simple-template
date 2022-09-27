05e991b6053c71b9f4a5fb5d742be1ef9da38d89
ph3 <100564304+ph3nac@users.noreply.github.com>
2022-09-27 16:56:39 +0900

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,19 @@ const path = require("node:path");
 const isDev = process.env.NODE_ENV === "development";
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 /** ↓ エディタで補完を効かせるための JSDoc */
 /** @type {import('webpack').Configuration} */
 module.exports = {
   mode: isDev ? "development" : "production",
   // dev モードではソースマップをつける
   devtool: isDev ? "source-map" : undefined,
-  entry: "./src/index.js",
   module: {
     rules: [
       {
         // 拡張子 js のファイル（正規表現）
         test: /\.js$/,
+        include: [path.resolve(__dirname, "src")],
         // ローダーの指定
         loader: "babel-loader",
       },
@@ -32,7 +33,7 @@ module.exports = {
       },
       {
         // 画像やフォントファイル
-        test: /\.(ico|png|svg|ttf|otf|eot|woff?2?)$/,
+        test: /\.(jpg|ico|png|svg|ttf|otf|eot|woff?2?)$/,
         type: "asset",
       },
     ],
@@ -41,11 +42,13 @@ module.exports = {
     static: {
       directory: "./dist",
     },
+    // https: true,
   },
+  entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "asset/[name][ext]",
+    assetModuleFilename: "assets/[name][ext]",
   },
   plugins: [
     new MiniCssExtractPlugin(),
@@ -54,5 +57,11 @@ module.exports = {
       inject: "head",
       scriptLoading: "blocking",
     }),
+    new CopyPlugin({
+      patterns: [{ from: "src/auto", to: "auto" }],
+    }),
   ],
+  stats: {
+    colors: true,
+  },
 };
